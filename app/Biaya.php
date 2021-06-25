74c894a61e9535b48d4e1934fa4c0c051eba2fa8
gunawankusno <gunawankusno72@gmail.com>
2021-06-25 09:37:57 +0800

    Web Tugas

---
+++ app/Biaya.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Biaya extends Model
+{
+    protected    $table       = 'biaya';
+    protected    $fillable    = ['id_biaya', 'jenis', 'biaya'];
+    public       $timestamps  = false;
+    protected    $primarykey  = 'id_biaya';
+}
